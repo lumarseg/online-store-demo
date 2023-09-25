703f7b68f34d75ce843eb7c2299fd67b1eb2c3c4
Luis Diego Maroto Segura <ldmaroto@gmail.com>
2023-09-25 16:48:36 -0600

    files updated

--- config/upload_s3.sh
+++ config/upload_s3.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # Compress folders into tar.gz files and display progress.
-tar zcfv cafe-v2.tar.gz cafe/
+tar zcfv store.tar.gz store/
 echo
-tar zcfv db-v2.tar.gz db/
+tar zcfv db.tar.gz db/
 echo
-tar zcfv setup-v2.tar.gz setup/
+tar zcfv setup.tar.gz setup/
 echo
 
 # Upload the tar.gz files to an Amazon S3 bucket.
-aws s3 cp cafe-v2.tar.gz s3://lumarseg-onlinestore-pub/ --acl public-read
-aws s3 cp db-v2.tar.gz s3://lumarseg-onlinestore-pub/ --acl public-read
-aws s3 cp setup-v2.tar.gz s3://lumarseg-onlinestore-pub/ --acl public-read
+aws s3 cp store.tar.gz s3://lumarseg-onlinestore-pub/ --acl public-read
+aws s3 cp db.tar.gz s3://lumarseg-onlinestore-pub/ --acl public-read
+aws s3 cp setup.tar.gz s3://lumarseg-onlinestore-pub/ --acl public-read
 
 # Remove the tar.gz files
-rm cafe-v2.tar.gz
-rm db-v2.tar.gz
-rm setup-v2.tar.gz
+rm store.tar.gz
+rm db.tar.gz
+rm setup.tar.gz
 
 echo
 echo Done !!!
